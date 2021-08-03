14287e861cffd1fb2d48b40bf74fd55bbc99881b
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-08-03 17:15:34 +1000

    Removed files that are no longer required

--- src/main/java/io/github/merchantpug/apugli/registry/ApugliPackets.java
+++ src/main/java/io/github/merchantpug/apugli/registry/ApugliPackets.java
@@ -1,15 +1,11 @@
 package io.github.merchantpug.apugli.registry;
 
 import io.github.merchantpug.apugli.networking.packet.EatGrassPacket;
-import io.github.merchantpug.apugli.networking.packet.LightUpBlockPacket;
-import io.github.merchantpug.apugli.networking.packet.RocketJumpPacket;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 
 public class ApugliPackets {
 
     public static void register() {
         ServerPlayNetworking.registerGlobalReceiver(EatGrassPacket.ID, EatGrassPacket::handle);
-        ServerPlayNetworking.registerGlobalReceiver(LightUpBlockPacket.ID, LightUpBlockPacket::handle);
-        ServerPlayNetworking.registerGlobalReceiver(RocketJumpPacket.ID, RocketJumpPacket::handle);
     }
 }
