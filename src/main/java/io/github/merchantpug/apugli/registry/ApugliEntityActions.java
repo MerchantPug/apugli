bdc59228f8fe0eab8731dba8a0d3d4a66f0652da
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-08-13 21:37:10 +1000

    Tweaked entity actions

--- src/main/java/io/github/merchantpug/apugli/registry/ApugliEntityActions.java
+++ src/main/java/io/github/merchantpug/apugli/registry/ApugliEntityActions.java
@@ -174,7 +174,7 @@ public class ApugliEntityActions {
                 .add("speed", SerializableDataTypes.DOUBLE, 1.0D)
                 .add("use_charged", SerializableDataTypes.BOOLEAN, false),
                 (data, entity) -> {
-                    if (entity instanceof LivingEntity && !entity.world.isClient()) {
+                    if (entity instanceof LivingEntity) {
                         double baseReach = 4.5D;
                         if (entity instanceof PlayerEntity) {
                             if (((PlayerEntity) entity).getAbilities().creativeMode) {
@@ -216,7 +216,9 @@ public class ApugliEntityActions {
                             float g = MathHelper.sin(entity.getPitch() * 0.017453292F);
                             float h = -MathHelper.cos(entity.getYaw() * 0.017453292F) * MathHelper.cos(entity.getPitch() * 0.017453292F);
 
-                            entity.world.createExplosion(entity, ApugliDamageSources.jumpExplosion((LivingEntity) entity), null, entityHitResult.getPos().getX(), entityHitResult.getPos().getY(), entityHitResult.getPos().getZ(), e, false, Explosion.DestructionType.NONE);
+                            if (!entity.world.isClient()) {
+                                entity.world.createExplosion(entity, ApugliDamageSources.jumpExplosion((LivingEntity) entity), null, entityHitResult.getPos().getX(), entityHitResult.getPos().getY(), entityHitResult.getPos().getZ(), e, false, Explosion.DestructionType.NONE);
+                            }
                             entity.addVelocity(f * data.getDouble("speed") * d, g * data.getDouble("speed") * d, h * data.getDouble("speed") * d);
                             entity.velocityModified = true;
                         } else if (blockHitResult != null && blockHitResult.getType() == HitResult.Type.BLOCK) {
@@ -227,7 +229,9 @@ public class ApugliEntityActions {
                             float g = MathHelper.sin(entity.getPitch() * 0.017453292F);
                             float h = -MathHelper.cos(entity.getYaw() * 0.017453292F) * MathHelper.cos(entity.getPitch() * 0.017453292F);
 
-                            entity.world.createExplosion(entity, ApugliDamageSources.jumpExplosion((LivingEntity) entity), null, blockHitResult.getPos().getX(), blockHitResult.getPos().getY(), blockHitResult.getPos().getZ(), e, false, Explosion.DestructionType.NONE);
+                            if (!entity.world.isClient()) {
+                                entity.world.createExplosion(entity, ApugliDamageSources.jumpExplosion((LivingEntity) entity), null, entityHitResult.getPos().getX(), entityHitResult.getPos().getY(), entityHitResult.getPos().getZ(), e, false, Explosion.DestructionType.NONE);
+                            }
                             entity.addVelocity(f * data.getDouble("speed") * d, g * data.getDouble("speed") * d, h * data.getDouble("speed") * d);
                             entity.velocityModified = true;
                         }
