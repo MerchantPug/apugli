94069231301424cbedd178de4c600a4175f19b3e
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-08-18 23:41:16 +1000

    Modified `apugli:raycast` to make it allow for null conditions

--- src/main/java/io/github/merchantpug/apugli/registry/ApugliEntityActions.java
+++ src/main/java/io/github/merchantpug/apugli/registry/ApugliEntityActions.java
@@ -66,7 +66,7 @@ public class ApugliEntityActions {
                 .add("target_condition", ApoliDataTypes.ENTITY_CONDITION, null)
                 .add("self_action", ApoliDataTypes.ENTITY_ACTION, null),
                 (data, entity) -> {
-                    if (entity instanceof LivingEntity) {
+                    if (entity instanceof LivingEntity && !entity.world.isClient()) {
                         double baseReach = 4.5D;
                         if (entity instanceof PlayerEntity) {
                             if (((PlayerEntity) entity).getAbilities().creativeMode) {
@@ -87,27 +87,27 @@ public class ApugliEntityActions {
                         Predicate<Entity> predicate = (entityx) -> !entityx.isSpectator() && entityx.collides();
                         EntityHitResult entityHitResult = ProjectileUtil.raycast(entity, vec3d, vec3d3, box, predicate, d);
                         BlockHitResult blockHitResult = entity.world.raycast(new RaycastContext(vec3d, vec3d3, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, entity));
-                        if (entityHitResult != null && entityHitResult.getEntity() instanceof LivingEntity && entityHitResult.getType() == HitResult.Type.ENTITY && data.isPresent("target_condition")) {
-                            Predicate<LivingEntity> entityCondition = (ConditionFactory<LivingEntity>.Instance) data.get("target_condition");
-                            boolean targetCondition = entityCondition.test((LivingEntity) entityHitResult.getEntity());
-                            if (targetCondition) {
-                                if (data.isPresent("target_action")) {
+                        if (entityHitResult != null && entityHitResult.getEntity() instanceof LivingEntity && entityHitResult.getType() == HitResult.Type.ENTITY) {
+                            if (data.isPresent("target_action")) {
+                                Predicate<LivingEntity> entityCondition = (ConditionFactory<LivingEntity>.Instance) data.get("target_condition");
+                                boolean targetCondition = entityCondition == null || entityCondition.test((LivingEntity) entityHitResult.getEntity());
+                                if (targetCondition) {
                                     ((ActionFactory<Entity>.Instance) data.get("target_action")).accept(entityHitResult.getEntity());
-                                }
-                                if (data.isPresent("self_action")) {
-                                    ((ActionFactory<Entity>.Instance) data.get("target_action")).accept(entity);
+                                    if (data.isPresent("self_action")) {
+                                        ((ActionFactory<Entity>.Instance) data.get("target_action")).accept(entity);
+                                    }
                                 }
                             }
-                        } else if (blockHitResult != null && blockHitResult.getType() == HitResult.Type.BLOCK && data.isPresent("block_condition")) {
-                            Predicate<CachedBlockPosition> blockCondition = (ConditionFactory<CachedBlockPosition>.Instance) data.get("block_condition");
-                            boolean targetCondition = blockCondition.test(new CachedBlockPosition(entity.world, blockHitResult.getBlockPos(), true));
-                            if (targetCondition) {
-                                if (data.isPresent("block_action")) {
+                        } else if (blockHitResult != null && blockHitResult.getType() == HitResult.Type.BLOCK) {
+                            if (data.isPresent("block_action")) {
+                                Predicate<CachedBlockPosition> blockCondition = (ConditionFactory<CachedBlockPosition>.Instance) data.get("block_condition");
+                                boolean targetCondition = blockCondition == null || blockCondition.test(new CachedBlockPosition(entity.world, blockHitResult.getBlockPos(), true));
+                                if (targetCondition) {
                                     ((ActionFactory<Triple<World, BlockPos, Direction>>.Instance) data.get("block_action")).accept(
                                             Triple.of(entity.world, blockHitResult.getBlockPos(), Direction.UP));
-                                }
-                                if (data.isPresent("self_action")) {
-                                    ((ActionFactory<Entity>.Instance) data.get("self_action")).accept(entity);
+                                    if (data.isPresent("self_action")) {
+                                        ((ActionFactory<Entity>.Instance) data.get("self_action")).accept(entity);
+                                    }
                                 }
                             }
                         }
@@ -122,117 +122,48 @@ public class ApugliEntityActions {
                 .add("amount", SerializableDataTypes.FLOAT, 0.0F),
                 (data, entity) -> {
                     if (entity instanceof LivingEntity && !entity.world.isClient()) {
-                            boolean useCharged = data.getBoolean("use_charged");
-                            boolean tmoCharged;
-                            boolean cursedCharged;
-                            float f = 1.0F;
-                            if (useCharged) {
-                                if (FabricLoader.getInstance().isModLoaded("toomanyorigins")) {
-                                    tmoCharged = ((LivingEntity)entity).hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("toomanyorigins", "charged")));
-                                } else tmoCharged = false;
-                                if (FabricLoader.getInstance().isModLoaded("cursedorigins")) {
-                                    cursedCharged = ((LivingEntity)entity).hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("cursedorigins", "charged")));
-                                } else cursedCharged = false;
-
-                                if (tmoCharged) {
-                                    ((LivingEntity)entity).removeStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("toomanyorigins", "charged")));
-                                    f = 2.0F;
-                                }
-                                if (cursedCharged) {
-                                    ((LivingEntity)entity).removeStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("cursedorigins", "charged")));
-                                    f = 2.0F;
-                                }
-                            }
-
-                            entity.world.createExplosion(entity, DamageSource.explosion((LivingEntity)entity), null, entity.getX(), entity.getY(), entity.getZ(), data.getFloat("radius") * f, false, (Explosion.DestructionType)data.get("behavior"));
-
-                            Collection<StatusEffectInstance> collection = ((LivingEntity)entity).getStatusEffects();
-                            if (!collection.isEmpty() && data.getBoolean("spawn_effect_cloud")) {
-                                AreaEffectCloudEntity areaEffectCloudEntity = new AreaEffectCloudEntity(entity.world, entity.getX(), entity.getY(), entity.getZ());
-                                areaEffectCloudEntity.setRadius(2.5F);
-                                areaEffectCloudEntity.setRadiusOnUse(-0.5F);
-                                areaEffectCloudEntity.setWaitTime(10);
-                                areaEffectCloudEntity.setDuration(areaEffectCloudEntity.getDuration() / 2);
-                                areaEffectCloudEntity.setRadiusGrowth(-areaEffectCloudEntity.getRadius() / (float)areaEffectCloudEntity.getDuration());
-
-                                for (StatusEffectInstance statusEffectInstance : collection) {
-                                    areaEffectCloudEntity.addEffect(new StatusEffectInstance(statusEffectInstance));
-                                }
-                                entity.world.spawnEntity(areaEffectCloudEntity);
-                            }
-                            DamageSource source = (DamageSource)data.get("source");
-                            float amount = data.getFloat("amount");
-                            if (source != null && amount != 0.0F) {
-                                entity.damage(source, amount);
-                            }
-                        }
-                    }));
-        register(new ActionFactory<>(Apugli.identifier("rocket_jump"), new SerializableData()
-                .add("source", SerializableDataTypes.DAMAGE_SOURCE, null)
-                .add("amount", SerializableDataTypes.FLOAT, 0.0F)
-                .add("speed", SerializableDataTypes.DOUBLE, 1.0D)
-                .add("use_charged", SerializableDataTypes.BOOLEAN, false),
-                (data, entity) -> {
-                    if (entity instanceof LivingEntity) {
-                        double baseReach = 4.5D;
-                        if (entity instanceof PlayerEntity) {
-                            if (((PlayerEntity) entity).getAbilities().creativeMode) {
-                                baseReach = 5.0D;
-                            }
-                        }
-                        double reach;
-                        if (FabricLoader.getInstance().isModLoaded("reach-entity-attributes")) {
-                            reach = ReachEntityAttributes.getReachDistance((LivingEntity) entity, baseReach);
-                        } else {
-                            reach = baseReach;
-                        }
-                        Vec3d vec3d = entity.getCameraPosVec(0.0F);
-                        Vec3d vec3d2 = entity.getRotationVec(0.0F);
-                        Vec3d vec3d3 = vec3d.add(vec3d2.x * reach, vec3d2.y * reach, vec3d2.z * reach);
-                        Box box = entity.getBoundingBox().stretch(vec3d2).expand(1.0D);
-                        double entityReach = reach * reach;
-                        Predicate<Entity> predicate = (entityx) -> !entityx.isSpectator() && entityx.collides();
-                        EntityHitResult entityHitResult = ProjectileUtil.raycast(entity, vec3d, vec3d3, box, predicate, entityReach);
-                        BlockHitResult blockHitResult = entity.world.raycast(new RaycastContext(vec3d, vec3d3, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, entity));
-
-                        DamageSource damageSource = (DamageSource)data.get("source");
-                        float damageAmount = data.getFloat("amount");
+                        boolean useCharged = data.getBoolean("use_charged");
                         boolean tmoCharged;
                         boolean cursedCharged;
-                        if (FabricLoader.getInstance().isModLoaded("toomanyorigins")) {
-                            tmoCharged = ((LivingEntity) entity).hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("toomanyorigins", "charged")));
-                        } else tmoCharged = false;
-                        if (FabricLoader.getInstance().isModLoaded("cursedorigins")) {
-                            cursedCharged = ((LivingEntity) entity).hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("cursedorigins", "charged")));
-                        } else cursedCharged = false;
-                        double d = (tmoCharged || cursedCharged) && data.getBoolean("use_charged") ? 1.5D : 1.0D;
-                        float e = (tmoCharged || cursedCharged) && data.getBoolean("use_charged") ? 2.0F : 1.5F;
-                        if (entityHitResult != null && entityHitResult.getEntity() instanceof LivingEntity && entityHitResult.getType() == HitResult.Type.ENTITY) {
-                            if (damageSource != null && damageAmount != 0.0F) {
-                                entity.damage(damageSource, damageAmount);
-                            }
-                            float f = MathHelper.sin(entity.getYaw() * 0.017453292F) * MathHelper.cos(entity.getPitch() * 0.017453292F);
-                            float g = MathHelper.sin(entity.getPitch() * 0.017453292F);
-                            float h = -MathHelper.cos(entity.getYaw() * 0.017453292F) * MathHelper.cos(entity.getPitch() * 0.017453292F);
+                        float f = 1.0F;
+                        if (useCharged) {
+                            if (FabricLoader.getInstance().isModLoaded("toomanyorigins")) {
+                                tmoCharged = ((LivingEntity)entity).hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("toomanyorigins", "charged")));
+                            } else tmoCharged = false;
+                            if (FabricLoader.getInstance().isModLoaded("cursedorigins")) {
+                                cursedCharged = ((LivingEntity)entity).hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("cursedorigins", "charged")));
+                            } else cursedCharged = false;
 
-                            if (!entity.world.isClient()) {
-                                entity.world.createExplosion(entity, ApugliDamageSources.jumpExplosion((LivingEntity) entity), null, blockHitResult.getPos().getX(), blockHitResult.getPos().getY(), blockHitResult.getPos().getZ(), e, false, Explosion.DestructionType.NONE);
-                                entity.addVelocity(f * data.getDouble("speed") * d, g * data.getDouble("speed") * d, h * data.getDouble("speed") * d);
-                                entity.velocityModified = true;
+                            if (tmoCharged) {
+                                ((LivingEntity)entity).removeStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("toomanyorigins", "charged")));
+                                f = 2.0F;
                             }
-                        } else if (blockHitResult != null && blockHitResult.getType() == HitResult.Type.BLOCK) {
-                            if (damageSource != null && damageAmount != 0.0F) {
-                                entity.damage(damageSource, damageAmount);
+                            if (cursedCharged) {
+                                ((LivingEntity)entity).removeStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("cursedorigins", "charged")));
+                                f = 2.0F;
                             }
-                            float f = MathHelper.sin(entity.getYaw() * 0.017453292F) * MathHelper.cos(entity.getPitch() * 0.017453292F);
-                            float g = MathHelper.sin(entity.getPitch() * 0.017453292F);
-                            float h = -MathHelper.cos(entity.getYaw() * 0.017453292F) * MathHelper.cos(entity.getPitch() * 0.017453292F);
+                        }
 
-                            if (!entity.world.isClient()) {
-                                entity.world.createExplosion(entity, ApugliDamageSources.jumpExplosion((LivingEntity) entity), null, blockHitResult.getPos().getX(), blockHitResult.getPos().getY(), blockHitResult.getPos().getZ(), e, false, Explosion.DestructionType.NONE);
-                                entity.addVelocity(f * data.getDouble("speed") * d, g * data.getDouble("speed") * d, h * data.getDouble("speed") * d);
-                                entity.velocityModified = true;
+                        entity.world.createExplosion(entity, DamageSource.explosion((LivingEntity)entity), null, entity.getX(), entity.getY(), entity.getZ(), data.getFloat("radius") * f, false, (Explosion.DestructionType)data.get("behavior"));
+
+                        Collection<StatusEffectInstance> collection = ((LivingEntity)entity).getStatusEffects();
+                        if (!collection.isEmpty() && data.getBoolean("spawn_effect_cloud")) {
+                            AreaEffectCloudEntity areaEffectCloudEntity = new AreaEffectCloudEntity(entity.world, entity.getX(), entity.getY(), entity.getZ());
+                            areaEffectCloudEntity.setRadius(2.5F);
+                            areaEffectCloudEntity.setRadiusOnUse(-0.5F);
+                            areaEffectCloudEntity.setWaitTime(10);
+                            areaEffectCloudEntity.setDuration(areaEffectCloudEntity.getDuration() / 2);
+                            areaEffectCloudEntity.setRadiusGrowth(-areaEffectCloudEntity.getRadius() / (float)areaEffectCloudEntity.getDuration());
+
+                            for (StatusEffectInstance statusEffectInstance : collection) {
+                                areaEffectCloudEntity.addEffect(new StatusEffectInstance(statusEffectInstance));
                             }
+                            entity.world.spawnEntity(areaEffectCloudEntity);
+                        }
+                        DamageSource source = (DamageSource)data.get("source");
+                        float amount = data.getFloat("amount");
+                        if (source != null && amount != 0.0F) {
+                            entity.damage(source, amount);
                         }
                     }
                 }));
