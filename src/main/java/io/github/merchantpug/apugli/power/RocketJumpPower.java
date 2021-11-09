b1f600fd25a9b4526fa3022114b2c9daa46d2559
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-11-09 21:26:52 +1100

    Added distance field to RocketJumpPower

--- src/main/java/io/github/merchantpug/apugli/power/RocketJumpPower.java
+++ src/main/java/io/github/merchantpug/apugli/power/RocketJumpPower.java
@@ -33,22 +33,22 @@ import net.minecraft.world.explosion.Explosion;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class RocketJumpPower extends ActiveCooldownPower {
     private Key key;
-    private DamageSource source;
-    private float amount;
-    private double speed;
-    private boolean useCharged;
+    private final double distance;
+    private final DamageSource source;
+    private final float amount;
+    private final double speed;
+    private final boolean useCharged;
     private final List<EntityAttributeModifier> modifiers = new LinkedList<>();
 
     public static PowerFactory<?> getFactory() {
         return new PowerFactory<RocketJumpPower>(Apugli.identifier("rocket_jump"),
                 new SerializableData()
                         .add("cooldown", SerializableDataTypes.INT)
+                        .add("distance", SerializableDataTypes.DOUBLE, Double.NaN)
                         .add("source", SerializableDataTypes.DAMAGE_SOURCE, null)
                         .add("amount", SerializableDataTypes.FLOAT, 0.0F)
                         .add("speed", SerializableDataTypes.DOUBLE, 1.0D)
@@ -59,7 +59,7 @@ public class RocketJumpPower extends ActiveCooldownPower {
                         .add("key", ApoliDataTypes.KEY, new Active.Key()),
                 (data) ->
                         (type, entity) ->  {
-                            RocketJumpPower power = new RocketJumpPower(type, entity, data.getInt("cooldown"), (HudRender)data.get("hud_render"), (DamageSource)data.get("source"), data.getFloat("amount"), data.getDouble("speed"), data.getBoolean("use_charged"));
+                            RocketJumpPower power = new RocketJumpPower(type, entity, data.getInt("cooldown"), (HudRender)data.get("hud_render"), data.getDouble("distance"), (DamageSource)data.get("source"), data.getFloat("amount"), data.getDouble("speed"), data.getBoolean("use_charged"));
                             power.setKey((Active.Key)data.get("key"));
                             if(data.isPresent("charged_modifier")) {
                                 power.addChargedJumpModifier(data.getModifier("charged_modifier"));
@@ -72,8 +72,9 @@ public class RocketJumpPower extends ActiveCooldownPower {
                 .allowCondition();
     }
 
-    public RocketJumpPower(PowerType<?> type, LivingEntity entity, int cooldownDuration, HudRender hudRender, DamageSource source, float amount, double speed, boolean useCharged) {
+    public RocketJumpPower(PowerType<?> type, LivingEntity entity, int cooldownDuration, HudRender hudRender, double distance, DamageSource source, float amount, double speed, boolean useCharged) {
         super(type, entity, cooldownDuration, hudRender, null);
+        this.distance = distance;
         this.source = source;
         this.amount = amount;
         this.speed = speed;
@@ -84,60 +85,48 @@ public class RocketJumpPower extends ActiveCooldownPower {
     public void onUse() {
         if (canUse()) {
             if (!entity.world.isClient()) {
-                double baseReach = 4.5D;
-                if (entity instanceof PlayerEntity) {
-                    if (((PlayerEntity) entity).getAbilities().creativeMode) {
-                        baseReach = 5.0D;
-                    }
-                }
-                double reach;
-                if (FabricLoader.getInstance().isModLoaded("reach-entity-attributes")) {
-                    reach = ReachEntityAttributes.getReachDistance((LivingEntity) entity, baseReach);
-                } else {
-                    reach = baseReach;
-                }
-                Vec3d vec3d = entity.getCameraPosVec(0.0F);
-                Vec3d vec3d2 = entity.getRotationVec(0.0F);
-                Vec3d vec3d3 = vec3d.add(vec3d2.x * reach, vec3d2.y * reach, vec3d2.z * reach);
-                Box box = entity.getBoundingBox().stretch(vec3d2).expand(1.0D);
-                double entityReach = reach * reach;
-                Predicate<Entity> predicate = (entityx) -> !entityx.isSpectator() && entityx.collides();
-                EntityHitResult entityHitResult = ProjectileUtil.raycast(entity, vec3d, vec3d3, box, predicate, entityReach);
-                BlockHitResult blockHitResult = entity.world.raycast(new RaycastContext(vec3d, vec3d3, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, entity));
-                boolean tmoCharged;
-                boolean cursedCharged;
-                if (FabricLoader.getInstance().isModLoaded("toomanyorigins")) {
-                    tmoCharged = ((LivingEntity) entity).hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("toomanyorigins", "charged")));
-                } else tmoCharged = false;
-                if (FabricLoader.getInstance().isModLoaded("cursedorigins")) {
-                    cursedCharged = ((LivingEntity) entity).hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("cursedorigins", "charged")));
-                } else cursedCharged = false;
+                double baseReach = (entity instanceof PlayerEntity && ((PlayerEntity) entity).getAbilities().creativeMode) ? 5.0D : 4.5D;
+                double reach = FabricLoader.getInstance().isModLoaded("reach-entity-attributes") ? ReachEntityAttributes.getReachDistance(entity, baseReach) : baseReach;
+                double distance = !Double.isNaN(this.distance) ? this.distance : reach;
+                Vec3d eyePosition = entity.getCameraPosVec(0);
+                Vec3d lookVector = entity.getRotationVec(0);
+                Vec3d traceEnd = eyePosition.add(lookVector.x * distance, lookVector.y * distance, lookVector.z * distance);
+                Box box = entity.getBoundingBox().stretch(lookVector).expand(1.0D);
+
+                RaycastContext context = new RaycastContext(eyePosition, traceEnd, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, entity);
+                BlockHitResult blockHitResult = entity.world.raycast(context);
+
+                double entityReach = blockHitResult != null ? blockHitResult.getBlockPos().getSquaredDistance(eyePosition.x, eyePosition.y, eyePosition.z, true) : distance * distance;
+                EntityHitResult entityHitResult = ProjectileUtil.raycast(entity, eyePosition, traceEnd, box, (traceEntity) -> !traceEntity.isSpectator() && traceEntity.collides(), entityReach);
+
+                HitResult.Type blockHitResultType = blockHitResult.getType();
+                HitResult.Type entityHitResultType = entityHitResult != null ? entityHitResult.getType() : null;
+                boolean tmoCharged = FabricLoader.getInstance().isModLoaded("toomanyorigins") && entity.hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("toomanyorigins", "charged")));
+                boolean cursedCharged = FabricLoader.getInstance().isModLoaded("cursedorigins") && entity.hasStatusEffect(Registry.STATUS_EFFECT.get(new Identifier("cursedorigins", "charged")));
                 boolean isCharged = tmoCharged || cursedCharged;
-                if (entityHitResult != null && entityHitResult.getType() == HitResult.Type.ENTITY) {
-                    this.handleRocketJump(entityHitResult, isCharged);
-                    this.use();
-                } else if (blockHitResult != null && blockHitResult.getType() == HitResult.Type.BLOCK) {
+
+                if (blockHitResultType == HitResult.Type.MISS && entityHitResultType == HitResult.Type.MISS) return;
+
+                if (blockHitResultType == HitResult.Type.BLOCK) {
                     this.handleRocketJump(blockHitResult, isCharged);
-                    this.use();
+                }
+
+                if (entityHitResultType == HitResult.Type.ENTITY) {
+                    this.handleRocketJump(entityHitResult, isCharged);
                 }
             }
         }
     }
 
     private void handleRocketJump(HitResult hitResult, boolean isCharged) {
-        double speed = this.speed;
-        if (isCharged && this.useCharged && !this.getChargedModifiers().isEmpty()) {
-            speed = modifyCharged(entity, RocketJumpPower.class, this.speed, null, null);
-        }
+        double speed = isCharged && this.useCharged && !this.getChargedModifiers().isEmpty() ? modifyCharged(entity, RocketJumpPower.class, this.speed) : this.speed;
         float e = isCharged && this.useCharged ? 2.0F : 1.5F;
-        if (this.source != null && this.amount != 0.0F) {
-            entity.damage(this.source, this.amount);
-        }
+        if (this.source != null && this.amount != 0.0F) entity.damage(this.source, this.amount);
         float f = MathHelper.sin(entity.getYaw() * 0.017453292F) * MathHelper.cos(entity.getPitch() * 0.017453292F);
         float g = MathHelper.sin(entity.getPitch() * 0.017453292F);
         float h = -MathHelper.cos(entity.getYaw() * 0.017453292F) * MathHelper.cos(entity.getPitch() * 0.017453292F);
 
-        entity.world.createExplosion(entity, ApugliDamageSources.jumpExplosion((LivingEntity) entity), null, hitResult.getPos().getX(), hitResult.getPos().getY(), hitResult.getPos().getZ(), e, false, Explosion.DestructionType.NONE);
+        entity.world.createExplosion(entity, ApugliDamageSources.jumpExplosion(entity), null, hitResult.getPos().getX(), hitResult.getPos().getY(), hitResult.getPos().getZ(), e, false, Explosion.DestructionType.NONE);
         entity.addVelocity(f * speed, g * speed, h * speed);
         entity.velocityModified = true;
         entity.fallDistance = 0;
@@ -161,7 +150,7 @@ public class RocketJumpPower extends ActiveCooldownPower {
         return modifiers;
     }
 
-    private static <T extends RocketJumpPower> double modifyCharged(Entity entity, Class<T> powerClass, double baseValue, Predicate<T> powerFilter, Consumer<T> powerAction) {
+    private static <T extends RocketJumpPower> double modifyCharged(Entity entity, Class<T> powerClass, double baseValue) {
         List<EntityAttributeModifier> modifiers = PowerHolderComponent.KEY.get(entity).getPowers(powerClass).stream()
                 .flatMap(p -> p.getChargedModifiers().stream()).collect(Collectors.toList());
         return (float) AttributeUtil.sortAndApplyModifiers(modifiers, baseValue);
