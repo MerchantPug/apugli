0c2c2afc315477c8d13956745d82b011986b8377
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-11-22 21:17:47 +1100

    Set remap to false for builds

--- src/main/java/io/github/merchantpug/apugli/mixin/ModifyDamageTakenPowerAccessor.java
+++ src/main/java/io/github/merchantpug/apugli/mixin/ModifyDamageTakenPowerAccessor.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-@Mixin(ModifyDamageTakenPower.class)
+@Mixin(value = ModifyDamageTakenPower.class, remap = false)
 public interface ModifyDamageTakenPowerAccessor {
     @Accessor
     Consumer<Entity> getSelfAction();
