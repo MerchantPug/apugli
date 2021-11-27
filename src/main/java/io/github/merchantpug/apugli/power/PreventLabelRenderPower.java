3b33b30a2d05ee998f6192d366b856ab4d8e926a
Pug <81167232+MerchantPug@users.noreply.github.com>
2021-11-27 21:30:21 +1100

    Updated PreventLabelRenderPower to new Apoli systems

--- src/main/java/io/github/merchantpug/apugli/power/PreventLabelRenderPower.java
+++ src/main/java/io/github/merchantpug/apugli/power/PreventLabelRenderPower.java
@@ -6,28 +6,33 @@ import io.github.apace100.apoli.power.PowerType;
 import io.github.apace100.apoli.power.factory.PowerFactory;
 import io.github.apace100.calio.data.SerializableData;
 import io.github.merchantpug.apugli.Apugli;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.Pair;
 
 import java.util.function.Predicate;
 
 public class PreventLabelRenderPower extends Power {
-    private final Predicate<LivingEntity> entityCondition;
+    private final Predicate<Entity> entityCondition;
+    private final Predicate<Pair<Entity, Entity>> biEntityCondition;
 
-    public PreventLabelRenderPower(PowerType<?> type, LivingEntity player, Predicate<LivingEntity> entityPredicate) {
+    public PreventLabelRenderPower(PowerType<?> type, LivingEntity player, Predicate<Entity> entityPredicate, Predicate<Pair<Entity, Entity>> biEntityPredicate) {
         super(type, player);
         this.entityCondition = entityPredicate;
+        this.biEntityCondition = biEntityPredicate;
     }
 
-    public boolean shouldHide(LivingEntity entity) {
-        return this.entityCondition == null || this.entityCondition.test(entity);
+    public boolean shouldHide(LivingEntity living) {
+        return (this.entityCondition == null || this.entityCondition.test(living)) && (this.biEntityCondition == null || this.biEntityCondition.test(new Pair<>(entity, living)));
     }
 
     public static PowerFactory<?> getFactory() {
         return new PowerFactory<PreventLabelRenderPower>(
                 Apugli.identifier("prevent_label_render"),
                 new SerializableData()
-                        .add("entity_condition", ApoliDataTypes.ENTITY_CONDITION, null),
-                data -> (type, player) -> new PreventLabelRenderPower(type, player, (Predicate<LivingEntity>) data.get("entity_condition")))
+                        .add("entity_condition", ApoliDataTypes.ENTITY_CONDITION, null)
+                        .add("bientity_condition", ApoliDataTypes.BIENTITY_CONDITION, null),
+                data -> (type, entity) -> new PreventLabelRenderPower(type, entity, (Predicate<Entity>) data.get("entity_condition"), (Predicate<Pair<Entity, Entity>>) data.get("bientity_condition")))
                 .allowCondition();
     }
 }
