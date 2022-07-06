a0c43b7a9a2b4c2e01e7b4bd2b4543bde2de118b
Pug <batterypug@gmail.com>
2022-07-07 01:09:11 +1000

    Fix GeckoLib incompatibility.

--- src/main/java/com/github/merchantpug/apugli/mixin/client/ArmorFeatureRendererMixin.java
+++ src/main/java/com/github/merchantpug/apugli/mixin/client/ArmorFeatureRendererMixin.java
@@ -30,7 +30,7 @@ public abstract class ArmorFeatureRendererMixin<T extends LivingEntity, M extend
         super(context);
     }
 
-    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At("HEAD"))
+    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", at = @At("TAIL"))
     private void renderModified(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
         List<ModifyEquippedItemRenderPower> modifyEquippedItemRenderPowers = PowerHolderComponent.getPowers(livingEntity, ModifyEquippedItemRenderPower.class);
         modifyEquippedItemRenderPowers.forEach(power -> {
