31e44a5881d21fed5c9e8dda963e25e7e303dc6b
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-02-16 02:30:17 +1100

    Add new `step_assist` field to `apugli:hover` power type.
    (cherry picked from commit 8d90afbccddcae216f7f78ba3570a9dab443f6c7)

--- src/main/java/net/merchantpug/apugli/power/HoverPower.java
+++ src/main/java/net/merchantpug/apugli/power/HoverPower.java
@@ -2,10 +2,34 @@ package net.merchantpug.apugli.power;
 
 import io.github.apace100.apoli.power.Power;
 import io.github.apace100.apoli.power.PowerType;
+import io.github.apace100.apoli.power.factory.PowerFactory;
+import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
+import net.merchantpug.apugli.Apugli;
 import net.minecraft.entity.LivingEntity;
 
 public class HoverPower extends Power {
-    public HoverPower(PowerType<?> type, LivingEntity entity) {
+    private final float correctionRange;
+
+    public HoverPower(PowerType<?> type, LivingEntity entity, float correctionRange) {
         super(type, entity);
+        this.correctionRange = correctionRange;
+    }
+
+    public static PowerFactory<?> getFactory() {
+        return new PowerFactory<HoverPower>(Apugli.identifier("hover"),
+                new SerializableData()
+                        .add("step_assist", SerializableDataTypes.FLOAT, 0.0F),
+                data ->
+                        (type, entity) -> new HoverPower(type, entity, data.getFloat("correction_range")))
+                .allowCondition();
+    }
+
+    public boolean canCorrectHeight() {
+        return correctionRange > 0.0F;
+    }
+
+    public float getCorrectionRange() {
+        return correctionRange;
     }
 }
