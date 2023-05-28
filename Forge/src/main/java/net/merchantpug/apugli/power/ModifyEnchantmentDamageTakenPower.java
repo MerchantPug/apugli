642c3dd89980a42f695f46bdba632636822f40b1
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-05-28 21:06:10 +1000

    Fix incorrect serializable data instances on Forge (#32).

--- Forge/src/main/java/net/merchantpug/apugli/power/ModifyEnchantmentDamageTakenPower.java
+++ Forge/src/main/java/net/merchantpug/apugli/power/ModifyEnchantmentDamageTakenPower.java
@@ -1,18 +1,16 @@
 package net.merchantpug.apugli.power;
 
 import com.google.auto.service.AutoService;
-import com.mojang.serialization.Codec;
 import io.github.edwinmindcraft.apoli.api.power.configuration.ConfiguredPower;
 import net.merchantpug.apugli.power.configuration.FabricValueModifyingConfiguration;
-import net.merchantpug.apugli.power.factory.ModifyBreedingCooldownPowerFactory;
-import net.merchantpug.apugli.power.factory.ModifyEnchantmentDamageDealtPowerFactory;
+import net.merchantpug.apugli.power.factory.ModifyEnchantmentDamagePowerFactory;
 import net.merchantpug.apugli.power.factory.ModifyEnchantmentDamageTakenPowerFactory;
 
 @AutoService(ModifyEnchantmentDamageTakenPowerFactory.class)
 public class ModifyEnchantmentDamageTakenPower extends AbstractValueModifyingPower implements ModifyEnchantmentDamageTakenPowerFactory<ConfiguredPower<FabricValueModifyingConfiguration, ?>> {
 
     public ModifyEnchantmentDamageTakenPower() {
-        super(ModifyBreedingCooldownPowerFactory.getSerializableData().xmap(
+        super(ModifyEnchantmentDamagePowerFactory.getSerializableData().xmap(
                 FabricValueModifyingConfiguration::new,
                 FabricValueModifyingConfiguration::data
         ).codec());
--- Forge/src/main/java/net/merchantpug/apugli/power/ModifySoulSpeedPower.java
+++ Forge/src/main/java/net/merchantpug/apugli/power/ModifySoulSpeedPower.java
@@ -1,21 +1,15 @@
 package net.merchantpug.apugli.power;
 
 import com.google.auto.service.AutoService;
-import com.mojang.serialization.Codec;
 import io.github.edwinmindcraft.apoli.api.power.configuration.ConfiguredPower;
 import net.merchantpug.apugli.power.configuration.FabricValueModifyingConfiguration;
-import net.merchantpug.apugli.power.factory.ModifyBreedingCooldownPowerFactory;
 import net.merchantpug.apugli.power.factory.ModifySoulSpeedPowerFactory;
-import net.minecraft.util.Tuple;
-import net.minecraft.world.entity.Entity;
-
-import java.util.function.Predicate;
 
 @AutoService(ModifySoulSpeedPowerFactory.class)
 public class ModifySoulSpeedPower extends AbstractValueModifyingPower implements ModifySoulSpeedPowerFactory<ConfiguredPower<FabricValueModifyingConfiguration, ?>> {
 
     public ModifySoulSpeedPower() {
-        super(ModifyBreedingCooldownPowerFactory.getSerializableData().xmap(
+        super(ModifySoulSpeedPowerFactory.getSerializableData().xmap(
                 FabricValueModifyingConfiguration::new,
                 FabricValueModifyingConfiguration::data
         ).codec());
