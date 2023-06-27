391402b079ad95abdf4e60f7fb3ca98c2cee373b
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-06-28 00:27:37 +1000

    Fix Forge server crash.

--- Common/src/main/java/net/merchantpug/apugli/util/TextureUtil.java
+++ Common/src/main/java/net/merchantpug/apugli/util/TextureUtil.java
@@ -1,10 +1,13 @@
 package net.merchantpug.apugli.util;
 
+import net.merchantpug.apugli.Apugli;
+import net.merchantpug.apugli.platform.Services;
 import net.merchantpug.apugli.power.TextureOrUrlPower;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class TextureUtil {
@@ -15,6 +18,7 @@ public class TextureUtil {
     }
 
     public static void cachePower(ResourceLocation powerId, TextureOrUrlPower power) {
-        CACHE.put(powerId, Triple.of(power.getUrlTextureIdentifier(), power.getTextureUrl(), power.getTextureLocation()));
+        ResourceLocation urlTextureLocation = new ResourceLocation(Apugli.ID, power.getPowerClassString().toLowerCase(Locale.ROOT) + "/" + powerId.getNamespace() + "/" + powerId.getPath());
+        CACHE.put(powerId, Triple.of(urlTextureLocation, power.getTextureUrl(), power.getTextureLocation()));
     }
 }
