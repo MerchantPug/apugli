ef722dee7983a27012fb86454ed752895f2728b2
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-06-27 22:21:05 +1000

    Fix #39

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 ### Bugfixes
+- Fixed `modify_enchantment_level` sometimes not applying when an ItemStack is empty. #39
 - Fixed elytra not rendering without power interference. #40
--- Common/src/main/java/net/merchantpug/apugli/mixin/xplatform/common/EnchantmentHelperMixin.java
+++ Common/src/main/java/net/merchantpug/apugli/mixin/xplatform/common/EnchantmentHelperMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -57,6 +56,21 @@ public abstract class EnchantmentHelperMixin {
     @ModifyVariable(method = "getItemEnchantmentLevel", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/item/ItemStack;getEnchantmentTags()Lnet/minecraft/nbt/ListTag;"))
     private static ListTag getEnchantmentsGetLevel(ListTag original) {
         return ApugliPowers.MODIFY_ENCHANTMENT_LEVEL.get().getEnchantments(apugli$itemEnchantmentLevelStack, original);
+    }
+
+    @Inject(method = "getEnchantmentLevel(Lnet/minecraft/world/item/enchantment/Enchantment;Lnet/minecraft/world/entity/LivingEntity;)I", at = @At("HEAD"), cancellable = true)
+    private static void getEnchantmentLevel(Enchantment enchantment, LivingEntity entity, CallbackInfoReturnable<Integer> cir) {
+        if (ApugliPowers.MODIFY_ENCHANTMENT_LEVEL.get().getEntityItemEnchants().containsKey(entity.getStringUUID())) {
+            int i = 0;
+            for (ItemStack stack : entity.getAllSlots()) {
+                ApugliPowers.MODIFY_ENCHANTMENT_LEVEL.get().getEnchantments(stack, stack.getEnchantmentTags());
+                int j = ApugliPowers.MODIFY_ENCHANTMENT_LEVEL.get().getItemEnchantmentLevel(enchantment, stack);
+                if (j > i) {
+                    i = j;
+                }
+            }
+            cir.setReturnValue(i);
+        }
     }
 
 }
