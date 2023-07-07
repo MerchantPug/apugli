119fa10608ea55f4fea625f23c55c9734aaf68f8
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-07-07 16:13:55 +1000

    Change `projectile_owner` action to generic `owner` action.

--- Common/src/main/java/net/merchantpug/apugli/registry/condition/ApugliBiEntityConditions.java
+++ Common/src/main/java/net/merchantpug/apugli/registry/condition/ApugliBiEntityConditions.java
@@ -2,8 +2,8 @@ package net.merchantpug.apugli.registry.condition;
 
 import net.merchantpug.apugli.condition.factory.IConditionFactory;
 import net.merchantpug.apugli.condition.factory.bientity.HitsOnTargetCondition;
+import net.merchantpug.apugli.condition.factory.bientity.OwnerCondition;
 import net.merchantpug.apugli.condition.factory.bientity.PrimeAdversaryCondition;
-import net.merchantpug.apugli.condition.factory.bientity.ProjectileOwnerCondition;
 import net.merchantpug.apugli.platform.Services;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.Entity;
@@ -12,8 +12,8 @@ public class ApugliBiEntityConditions {
     
     public static void registerAll() {
         register("hits_on_target",  new HitsOnTargetCondition());
+        register("owner", new OwnerCondition());
         register("prime_adversary", new PrimeAdversaryCondition());
-        register("projectile_owner", new ProjectileOwnerCondition());
     }
     
     private static void register(String name, IConditionFactory<Tuple<Entity, Entity>> factory) {
