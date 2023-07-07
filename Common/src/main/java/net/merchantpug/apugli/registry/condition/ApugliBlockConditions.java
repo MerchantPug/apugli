3d78c8404d005ae1a2c43f7f8412df2b52e3ff6b
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-07-08 06:02:17 +1000

    Add `raining` and `thundering` conditions.

--- Common/src/main/java/net/merchantpug/apugli/registry/condition/ApugliBlockConditions.java
+++ Common/src/main/java/net/merchantpug/apugli/registry/condition/ApugliBlockConditions.java
@@ -1,7 +1,10 @@
 package net.merchantpug.apugli.registry.condition;
 
 import net.merchantpug.apugli.condition.factory.IConditionFactory;
+import net.merchantpug.apugli.condition.factory.block.InRainCondition;
 import net.merchantpug.apugli.condition.factory.block.IsAirCondition;
+import net.merchantpug.apugli.condition.factory.block.RainingCondition;
+import net.merchantpug.apugli.condition.factory.block.ThunderingCondition;
 import net.merchantpug.apugli.platform.Services;
 import net.minecraft.world.level.block.state.pattern.BlockInWorld;
 
@@ -9,6 +12,9 @@ public class ApugliBlockConditions {
     
     public static void registerAll() {
         register("air", new IsAirCondition());
+        register("in_rain", new InRainCondition());
+        register("raining", new RainingCondition());
+        register("thundering", new ThunderingCondition());
     }
     
     private static void register(String name, IConditionFactory<BlockInWorld> factory) {
