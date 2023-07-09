7afd20d0f8a35475feb9810613e71e7f7381ac75
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-07-10 09:05:25 +1000

    Fix crash with doesApply method.

--- Common/src/main/java/net/merchantpug/apugli/power/factory/ModifyBreedingCooldownPowerFactory.java
+++ Common/src/main/java/net/merchantpug/apugli/power/factory/ModifyBreedingCooldownPowerFactory.java
@@ -2,12 +2,8 @@ package net.merchantpug.apugli.power.factory;
 
 import io.github.apace100.calio.data.SerializableData;
 import net.merchantpug.apugli.platform.Services;
-import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-
-import java.io.Serial;
-import java.util.function.Predicate;
 
 public interface ModifyBreedingCooldownPowerFactory<P> extends ValueModifyingPowerFactory<P> {
     
@@ -18,7 +14,7 @@ public interface ModifyBreedingCooldownPowerFactory<P> extends ValueModifyingPow
 
     default boolean doesApply(P power, LivingEntity entity, Entity animal) {
         SerializableData.Instance data = getDataFromPower(power);
-        return Services.CONDITION.checkBiEntity(data, data.get("bientity_condition"), entity, animal);
+        return Services.CONDITION.checkBiEntity(data, "bientity_condition", entity, animal);
     }
     
 }
