5c86739f5e292b0667b543ad1a8155cf578153c1
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-07-15 05:22:51 +1000

    Add RaycastBetweenAction

--- Common/src/main/java/net/merchantpug/apugli/registry/action/ApugliBiEntityActions.java
+++ Common/src/main/java/net/merchantpug/apugli/registry/action/ApugliBiEntityActions.java
@@ -2,6 +2,7 @@ package net.merchantpug.apugli.registry.action;
 
 import net.merchantpug.apugli.action.factory.IActionFactory;
 import net.merchantpug.apugli.action.factory.bientity.ChangeHitsOnTargetAction;
+import net.merchantpug.apugli.action.factory.bientity.RaycastBetweenAction;
 import net.merchantpug.apugli.action.factory.bientity.SpawnCustomEffectCloudAction;
 import net.merchantpug.apugli.action.factory.bientity.meta.PacketAction;
 import net.merchantpug.apugli.platform.Services;
@@ -12,6 +13,7 @@ public class ApugliBiEntityActions {
     
     public static void registerAll() {
         register("change_hits_on_target", new ChangeHitsOnTargetAction());
+        register("raycast_between", new RaycastBetweenAction());
         register("spawn_custom_effect_cloud", new SpawnCustomEffectCloudAction());
         register("packet", new PacketAction());
     }
