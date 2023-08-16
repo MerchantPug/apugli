f72e59cf07ceacea73c3f63f57a9c314514b3dbc
MerchantPug <81167232+MerchantPug@users.noreply.github.com>
2023-08-16 21:42:05 +1000

    Fix FreezePower crash on Forge.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
 ### Bugfixes
-- Fix `freeze` power not applying speed modifiers whilst not on the ground.
+- Fixed `freeze` power not applying speed modifiers whilst not on the ground.
+- Fixed `freeze` power crash on Forge.
--- Forge/src/main/java/net/merchantpug/apugli/power/FreezePower.java
+++ Forge/src/main/java/net/merchantpug/apugli/power/FreezePower.java
@@ -4,13 +4,12 @@ import com.google.auto.service.AutoService;
 import io.github.edwinmindcraft.apoli.api.power.configuration.ConfiguredPower;
 import net.merchantpug.apugli.power.configuration.FabricValueModifyingConfiguration;
 import net.merchantpug.apugli.power.factory.FreezePowerFactory;
-import net.merchantpug.apugli.power.factory.ModifyDurabilityChangePowerFactory;
 
 @AutoService(FreezePowerFactory.class)
 public class FreezePower extends AbstractValueModifyingPower implements FreezePowerFactory<ConfiguredPower<FabricValueModifyingConfiguration, ?>>  {
 
     public FreezePower() {
-        super(ModifyDurabilityChangePowerFactory.getSerializableData().xmap(
+        super(FreezePowerFactory.getSerializableData().xmap(
                 FabricValueModifyingConfiguration::new,
                 FabricValueModifyingConfiguration::data
         ).codec());
