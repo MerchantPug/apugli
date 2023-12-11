bf477340a3738addb51ac9b79abad9d4f47594bc
Pug <81167232+MerchantPug@users.noreply.github.com>
2023-12-12 01:31:46 +1100

    Fix incorrectly merged content.

--- common/src/main/java/net/merchantpug/apugli/mixin/xplatform/common/ServerPlayNetworkHandlerMixin.java
+++ common/src/main/java/net/merchantpug/apugli/mixin/xplatform/common/ServerPlayNetworkHandlerMixin.java
@@ -1,29 +1,18 @@
 package net.merchantpug.apugli.mixin.xplatform.common;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.merchantpug.apugli.network.s2c.UpdateUrlTexturesPacket;
 import net.merchantpug.apugli.platform.Services;
 import net.merchantpug.apugli.registry.power.ApugliPowers;
-import net.merchantpug.apugli.util.TextureUtil;
-import net.minecraft.network.protocol.game.ServerboundResourcePackPacket;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public class ServerPlayNetworkHandlerMixin {
     @Shadow public ServerPlayer player;
-
-    @Shadow @Final private MinecraftServer server;
 
     @ModifyExpressionValue(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;clientIsFloating:Z", ordinal = 0))
     private boolean apugli$doNotKickIfUsingHoverPower(boolean original) {
@@ -51,12 +40,6 @@ public class ServerPlayNetworkHandlerMixin {
             return false;
         }
         return original;
-    }
-
-    @Inject(method = "handleResourcePackResponse", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/game/ServerboundResourcePackPacket;getAction()Lnet/minecraft/network/protocol/game/ServerboundResourcePackPacket$Action;"))
-    private void apugli$sendUrlTexturesToPlayerAfterResourceLoad(ServerboundResourcePackPacket packet, CallbackInfo ci) {
-        if (packet.getAction() == ServerboundResourcePackPacket.Action.DECLINED && this.server.isResourcePackRequired()) return;
-        Services.PLATFORM.sendS2C(new UpdateUrlTexturesPacket(TextureUtil.getCache()), player);
     }
 
 }
