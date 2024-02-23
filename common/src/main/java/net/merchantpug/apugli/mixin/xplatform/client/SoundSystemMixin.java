f25829f9fabc7be1d11109c59ebf844e95d492fe
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-02-24 04:03:36 +1100

    Check to make sure sounds are loaded before attempting cancelling sound.

--- common/src/main/java/net/merchantpug/apugli/mixin/xplatform/client/SoundSystemMixin.java
+++ common/src/main/java/net/merchantpug/apugli/mixin/xplatform/client/SoundSystemMixin.java
@@ -8,15 +8,18 @@ import net.minecraft.client.resources.sounds.AbstractSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.SoundEngine;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundEngine.class)
 public class SoundSystemMixin {
+    @Shadow private boolean loaded;
+
     @Inject(method = "play", at = @At("HEAD"), cancellable = true)
     private void apugli$cancelSound(SoundInstance sound, CallbackInfo ci) {
-        if(sound instanceof AbstractSoundInstance abstractSound && Services.POWER.getPowers(Minecraft.getInstance().player, ApugliPowers.PREVENT_SOUND.get()).stream().anyMatch(power -> (power.doesApplyToCategory(sound.getSource()) || power.doesApplyToSound(((AbstractSoundInstanceAccess)abstractSound).apugli$getSoundEvent())) && power.isSoundNotWhitelisted(((AbstractSoundInstanceAccess)abstractSound).apugli$getSoundEvent()))) {
+        if(this.loaded && sound instanceof AbstractSoundInstance abstractSound && Services.POWER.getPowers(Minecraft.getInstance().player, ApugliPowers.PREVENT_SOUND.get()).stream().anyMatch(power -> (power.doesApplyToCategory(sound.getSource()) || power.doesApplyToSound(((AbstractSoundInstanceAccess)abstractSound).apugli$getSoundEvent())) && power.isSoundNotWhitelisted(((AbstractSoundInstanceAccess)abstractSound).apugli$getSoundEvent()))) {
             ci.cancel();
         }
     }
